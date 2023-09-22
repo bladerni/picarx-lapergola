31fd6bf8f9eb0c28f1d696ade152bf86bb13cf29
Jaime Diaz <jaime.diaz@betterask.erni>
2023-09-22 17:03:06 +0200

    Commands finished

--- firebase/robot/listener.py
+++ firebase/robot/listener.py
@@ -1,5 +1,3 @@
-import requests
-import time
 from firebase_admin import credentials, initialize_app, db
 
 cred = credentials.Certificate("./serviceAccountKey.json")
@@ -7,7 +5,23 @@ initialize_app(cred, {
     'databaseURL' : 'https://erni-hackathon-default-rtdb.europe-west1.firebasedatabase.app'
 })
 
-def listener(event):
-    print(event.data)  # new data at /reference/event.path. None if deleted
+def accelerationListener(event):
+    print(event.data)
 
-ref = db.reference('/accelerate').listen(listener)
+def carDirectionListener(event):
+    print(event.data)
+
+def carDiscoModeListener(event):
+    print(event.data)
+
+def carAutomaticModeListener(event):
+    print(event.data)
+
+def cameraDirectionListener(event):
+    print(event.data)
+
+ref = db.reference('/car/acceleration').listen(accelerationListener)
+ref = db.reference('/car/direction').listen(carDirectionListener)
+ref = db.reference('/car/disco_mode').listen(carDiscoModeListener)
+ref = db.reference('/car/automatic_mode').listen(carAutomaticModeListener)
+ref = db.reference('/camera/direction').listen(cameraDirectionListener)
