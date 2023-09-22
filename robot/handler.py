ffb11323ea6961720b609f025ab4ee4090cf9045
Jaime Diaz <jaime.diaz@betterask.erni>
2023-09-22 21:07:40 +0200

    First raspberry movement

--- robot/handler.py
+++ robot/handler.py
@@ -1,3 +1,5 @@
+# #!/usr/bin/env python3
+from picarx import Picarx
 from firebase_admin import credentials, initialize_app, db
 
 cred = credentials.Certificate("./serviceAccountKey.json")
@@ -5,11 +7,27 @@ initialize_app(cred, {
     'databaseURL' : 'https://erni-hackathon-default-rtdb.europe-west1.firebasedatabase.app'
 })
 
+speed = 0
+angle = 0
+
+px = Picarx()
+
+def move(speed, angle):
+    px.set_dir_servo_angle(angle)
+    if speed == 0:
+        px.stop()
+    if speed > 0:
+        px.forward(speed)
+    else:
+        px.backward(speed)
+
 def acceleration_listener(event):
-    print(event.data)
+    speed = event.data
+    move(speed, angle)
 
 def car_direction_listener(event):
-    print(event.data)
+    angle = event.data
+    move(speed, angle)
 
 def car_disco_mode_listener(event):
     print(event.data)
